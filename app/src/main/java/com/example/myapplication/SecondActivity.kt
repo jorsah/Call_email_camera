b2bb5885543c97ae0416642d94794651ad199e19
jorsah <jor_sah@mail.ru>
2021-03-21 10:51:11 +0400

    Hw3

--- app/src/main/java/com/example/myapplication/SecondActivity.kt
+++ app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -1,9 +1,13 @@
 package com.example.myapplication
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+
 
 class SecondActivity: AppCompatActivity() {
     @SuppressLint("SetTextI18n", "ResourceAsColor")
@@ -14,12 +18,33 @@ class SecondActivity: AppCompatActivity() {
         val name = intent.getStringExtra("name")
         val surname = intent.getStringExtra("surname")
         val phone = intent.getStringExtra("phone")
-        val email = intent.getStringExtra("email")
+        val email1 = intent.getStringExtra("email")
 
-        val call = findViewById<Button>(R.id.callBt)
 
-        call.setOnClickListener {
-            call.text = name
+        val callBt = findViewById<Button>(R.id.callBt)
+        val emailBt = findViewById<Button>(R.id.emailBt)
+        val cameraBt = findViewById<Button>(R.id.cameraBt)
+
+        callBt.setOnClickListener {
+            val intent1 = Intent()
+            val uri = Uri.parse("tel:$phone")
+            intent1.action = Intent.ACTION_VIEW
+            intent1.data = uri
+            startActivity(intent1)
+        }
+
+        emailBt.setOnClickListener {
+            val email = Intent(Intent.ACTION_SEND)
+            email.putExtra(Intent.EXTRA_EMAIL, arrayOf(email1))
+            email.putExtra(Intent.EXTRA_SUBJECT, "$name $surname")
+            email.putExtra(Intent.EXTRA_TEXT,"fdsa")
+            email.type = "message/rfc822"
+            startActivity(Intent.createChooser(email, "Choose an Email client :"))
+        }
+
+        cameraBt.setOnClickListener {
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(cameraIntent)
         }
     }
 }
